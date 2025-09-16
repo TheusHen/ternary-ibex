a7413b046350511d0e6fe2f650c109e5aa74efe1
I'm Matheus <codelong@proton.me>
2025-09-16 00:56:34 +0000

    Fix formatting of align_mem macro in core_portme.h

--- examples/sw/benchmarks/coremark/ibex/core_portme.h
+++ examples/sw/benchmarks/coremark/ibex/core_portme.h
@@ -91,7 +91,7 @@ typedef size_t ee_size_t;
         This macro is used to align an offset to point to a 32b value. It is
    used in the Matrix algorithm to initialize the input memory blocks.
 */
-#define align_mem(x) (void *)(4 + (((ee_ptr_int)(x) - 1) & ~3))
+#define align_mem(x) (void *)(4 + (((ee_ptr_int)(x)-1) & ~3))
 
 /* Configuration : CORE_TICKS
         Define type of return from the timing functions.
